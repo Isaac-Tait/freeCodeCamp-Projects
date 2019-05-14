ac71124bfc391b90b035b6506f2d3261173d50f2
Isaac Tait <isaacmtait@gmail.com>
2019-05-14 08:03:36 -0700

    Assignment w/ a returned value

---
+++ wrapping my mind.js
@@ -0,0 +1,12 @@
+var changed = 0;
+
+// the function "change" is defined as a number
+function change(num) {
+    // the number has five added to it then divided by three
+  return (num + 5) / 3;
+}
+// the number in line 4 is defined as 10. 10 + 5 = 15. 15 / 3 is equal to 5. 
+changed = change(10);
+
+//Therefore the variable "changed" in line 1 is equal to 5.
+console.log(changed);
