dcaad261509a38bf37923baaba3ad6a1bf9a569f
Isaac Tait <isaacmtait@gmail.com>
2019-05-27 14:23:27 -0700

    golf par challenge

---
+++ par.js
@@ -0,0 +1,20 @@
+var names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+function golfScore(par, strokes) {
+    if (strokes = 1) {
+      return "Hole-in-one!";
+    } else if (golfScore = 2) {
+      return "Eagle";
+    } else if (golfScore = 1) {
+      return "Birdie";
+    } else if (golfScore = 0) {
+      return "Par";
+    } else if (golfScore = -1) {
+      return "Bogey";
+    } else if (golfScore = -2) {
+      return "Double Bogey";
+    } if (golfScore = -3) { 
+        return "Go Home!";
+    }
+  };
+
+  console.log(golfScore(3, 1));
