c2458cc6cc54ddac2df6650c061199ee5f57897a
Isaac Tait <isaacmtait@gmail.com>
2019-06-07 15:25:45 -0700

    added URL for reference & personal input

--- return-early-pattern-for-functions.js
+++ return-early-pattern-for-functions.js
@@ -1,3 +1,6 @@
+//Setup
+https://learn.freecodecamp.org/javascript-algorithms-and-data-structures/basic-javascript/return-early-pattern-for-functions
+
 function abTest(a, b) {
     if (a < 0 || b < 0) {
       return undefined;
@@ -8,3 +11,6 @@ function abTest(a, b) {
     
     // Change values below to test your code
     console.log(abTest(12,2));
+
+    // I figured this out on my own from the following instructions:
+    // Modify the function abTest so that if a or b are less than 0 the function will immediately exit with a value of undefined.
