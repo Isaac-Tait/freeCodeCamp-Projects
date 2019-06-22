0c6437ee01693c2224fffc7823302752ecf95033
IsaacTait <IsaacTait@Isaacs-MacBook-Air.local>
2019-06-22 15:41:55 -0700

    edited syntax for clarity

--- confused.js
+++ confused.js
@@ -1,9 +1,12 @@
 var someObj = {
     propName: "John"
   };
+
   function propPrefix(str) {
     var s = "prop";
     return s + str;
   }
-  var someProp = propPrefix("Name"); // someProp now holds the value 'propName'
+
+  var someProp = propPrefix("Name");// someProp now holds the value 'propName'
+  
   console.log(someObj[someProp]); // "John"
